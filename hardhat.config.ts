1644abbbb6fbfefe0a731cc25c97f05f6f299f86
sichengwen <sichengwen@kuaishou.com>
2023-08-15 15:33:31 +0800

    init hardhat project

---
+++ hardhat.config.ts
@@ -0,0 +1,8 @@
+import { HardhatUserConfig } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+
+const config: HardhatUserConfig = {
+  solidity: "0.5.0",
+};
+
+export default config;
