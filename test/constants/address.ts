79dc225f3765d3c6ffd48f45ea72ebfcd4f00928
sichengwen <sichengwen@kuaishou.com>
2023-09-06 15:30:11 +0800

    add mark

--- test/constants/address.ts
+++ test/constants/address.ts
@@ -6,8 +6,12 @@ export const aDAIAddress = '0xfC1E690f61EFd961294b3e1Ce3313fBD8aa4f85d';
 export const aUSDCAddress = '0x9bA00D6856a4eDF4665BcA2C2309936572473B7E';
 export const aUSDTAddress = '0x71fc860F7D3A592A4a98740e39dB31d25db65ae8';
 
-export const DAIAddress = '0x6b175474e89094c44da98b954eedeac495271d0f';
-export const USDCAddress = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48';
-export const USDTAddress = '0xdac17f958d2ee523a2206206994597c13d831ec7';
+export const daiAddress = '0x6b175474e89094c44da98b954eedeac495271d0f';
+export const usdcAddress = '0xa0b86991c6218b36c1d19d4a2e9eb0ce3606eb48';
+export const usdtAddress = '0xdac17f958d2ee523a2206206994597c13d831ec7';
+
+export const daiRicherAddress = '0xbf702ea18bb1ab2a710394993a576ec61476ccf3';
+export const usdcRicherAddress = '0x18F4638b52232d8780b18115dBa89cce1c396Eeb';
+export const usdtRicherAddress = '0x8558FE88F8439dDcd7453ccAd6671Dfd90657a32';
 
 export const impersonateAccount = '0xebb17ec2bce083605a9a665cbd905ece11e5498a';
